1923c865a07bec3e8eb37d1d4a2a6ec0a1824504
azamn <azam.noor@techswevil.com>
2023-04-07 15:33:39 +0500

    task name updated in working branch.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'task 3',
+      title: 'task 33',
       theme: ThemeData(
         // This is the theme of your application.
         //
